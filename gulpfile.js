4ddfd5dd682c26f91fb06770a8732fc66eec52d3
gambitok <gambitokgd@gmail.com>
2020-04-16 16:31:43 +0300

    init Gulp, Sass, Browsersync

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(done) {
+    gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+    done();
+});
+
+gulp.task('serve', function(done) {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("sass/*.sass", gulp.series('sass'));
+    gulp.watch("*.html").on('change', () => {
+        browserSync.reload();
+        done();
+    });
+    done();
+});
+
+gulp.task('default', gulp.series('sass', 'serve'));
